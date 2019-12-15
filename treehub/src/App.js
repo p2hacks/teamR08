6297ae08dceec81bc97402fe404968071a3fecb5
lowene <taketake3322@icloud.com>
2019-12-15 10:47:35 +0900

    これが本当に最後

--- treehub/src/App.js
+++ treehub/src/App.js
@@ -23,11 +23,17 @@ import present from './images/present.png';
 import belllease from './images/belllease.png';
 import light1 from './images/light1.png';
 import light2 from './images/light2.png';
+import light3 from './images/light3.png';
+import light4 from './images/light4.png';
+import light5 from './images/light5.png';
+import light6 from './images/light6.png';
+import light7 from './images/light7.png';
+import light8 from './images/light8.png';
+import light9 from './images/light9.png';
+import zinzya from './images/zinzya.png';
 
-
-var DataImages = new Array(ccc,ccc,ccc,ccc,ccc,ccc,ccc,ccc,ccc,ccc,ccc,ccc,ccc);
-var images = new Array(SaveButton,SaveButton,SaveButton,ccc,ccc,ccc,ccc,ccc,ccc,ccc,toumei,joucho,jk);
-var MenuImages = new Array(bell,lease,santahat,snowman,socks,present,belllease,light1,light2)
+var DataImages = new Array(bell,bell,bell,bell,bell,bell,bell,bell,bell,bell,bell,bell,bell);
+var MenuImages = new Array(bell,lease,santahat,snowman,socks,present,belllease,light1,light2,light3,light4,light5,light6,light7,light8,light9,zinzya)
 
 
 
@@ -50,6 +56,8 @@ function Edit(){
   const [image11,setimage11] = React.useState(DataImages[10]);
   const [image12,setimage12] = React.useState(DataImages[11]);
   const [image13,setimage13] = React.useState(DataImages[12]);
+
+
   
 
   function click_recieve(i) {//受け取りの関数。iが配列の番号
@@ -96,6 +104,25 @@ function Edit(){
     function image13_send(){
     setimage13(image_state);
   }
+  
+  function Save(){
+    DataImages[0] = image1;
+    DataImages[1] = image2;
+    DataImages[2] = image3;
+    DataImages[3] = image4;
+    DataImages[4] = image5;
+    DataImages[5] = image6;
+    DataImages[6] = image7;
+    DataImages[7] = image8;
+    DataImages[8] = image9;
+    DataImages[9] = image10;
+    DataImages[10] = image11;
+    DataImages[11] = image12;
+    DataImages[12] = image13;
+    console.log(DataImages[0]);
+    window.location.reload()
+  }
+
   function menuChange(){
     setCheck(!menucheck);
   }
@@ -133,12 +160,18 @@ function Edit(){
             <li><input type="image" src={MenuImages[6]} className="Edit_menu_img"onClick = { () => click_recieve(6)}></input></li>   
             <li><input type="image" src={MenuImages[7]} className="Edit_menu_img"onClick = { () => click_recieve(7)}></input></li>            
             <li><input type="image" src={MenuImages[8]} className="Edit_menu_img"onClick = { () => click_recieve(8)}></input></li>
+            <li><input type="image" src={MenuImages[9]} className="Edit_menu_img"onClick = { () => click_recieve(9)}></input></li>
+            <li><input type="image" src={MenuImages[10]} className="Edit_menu_img"onClick = { () => click_recieve(10)}></input></li>
+            <li><input type="image" src={MenuImages[11]} className="Edit_menu_img"onClick = { () => click_recieve(11)}></input></li>
+            <li><input type="image" src={MenuImages[12]} className="Edit_menu_img"onClick = { () => click_recieve(12)}></input></li>
+            <li><input type="image" src={MenuImages[13]} className="Edit_menu_img"onClick = { () => click_recieve(13)}></input></li>
+            <li><input type="image" src={MenuImages[14]} className="Edit_menu_img"onClick = { () => click_recieve(14)}></input></li>
+            <li><input type="image" src={MenuImages[15]} className="Edit_menu_img"onClick = { () => click_recieve(15)}></input></li>
+            <li><input type="image" src={MenuImages[16]} className="Edit_menu_img"onClick = { () => click_recieve(16)}></input></li>            
           </ul>
           <input  type="image" src={MenuButton} className={`${menucheck ? 'MenuButton_after':'MenuButton_before'}`} onClick ={menuChange} ></input>
          </div>
-         <div className="pose">
-        <button className="poseButton">メニュー</button>
-        </div>
+          <button className="poseButton" onClick={Save}>セーブ</button>
       </div>
     </section>
     
